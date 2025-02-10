afe57fd03707cfb79dd283e2d680ca3bcb20ee73
ilhamhatta <milhamhat@gmail.com>
2025-02-10 22:35:09 +0700

    konfigurasi Next.js untuk mengatasi Error: Pages only supports files up to 25 MiB in size

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Pastikan output dalam bentuk HTML statis untuk Cloudflare
+  images: {
+    unoptimized: true, // Disable image optimization bawaan Next.js
+  },
+};
 
 export default nextConfig;
