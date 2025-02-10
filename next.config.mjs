d787cd8e5a0ad28115a3e098b076f4a8fa8f76a5
ilhamhatta <milhamhat@gmail.com>
2025-02-11 03:24:45 +0700

    install critters untuk optimasi deploy css

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // Gunakan mode static untuk Cloudflare Pages
-  experimental: {
-    optimizeCss: true,
-    optimizeFonts: true,
-    scrollRestoration: true,
-    externalDir: true,
-  },
-  images: {
-    formats: ["image/avif", "image/webp"], // Gunakan format gambar lebih kecil
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
